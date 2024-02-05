fa55bd7a4c0ddc4703ab5a33b654d61199a9864f
JcbSm <jacob.ted.seaman@gmail.com>
2024-02-05 14:32:34 +0000

    feat: Added agent icons & emojis

--- src/lib/valorant/Agents.ts
+++ src/lib/valorant/Agents.ts
@@ -70,29 +70,30 @@ export class Sentinel extends Agent {
 }
 
 export const agents = [
-    new Duelist('Jett', '', true),
-    new Duelist('Phoenix', '', true),
-    new Duelist('Reyna', ''),
-    new Duelist('Yoru', ''),
-    new Duelist('Neon', ''),
-    new Duelist('Iso', ''),
+    new Duelist('Jett', '930526575366111243', true),
+    new Duelist('Phoenix', '930526575408070766', true),
+    new Duelist('Raze', '930526575789760512'),
+    new Duelist('Reyna', '930526575395483739'),
+    new Duelist('Yoru', '930526575953346560'),
+    new Duelist('Neon', '936618293417115698'),
+    new Duelist('Iso', '1204070638432817184'),
 
-    new Initiator('Sova', '', true),
-    new Initiator('Breach', ''),
-    new Initiator('Skye', ''),
-    new Initiator('KAY/O', ''),
-    new Initiator('Fade', ''),
-    new Initiator('Gecko', ''),
+    new Initiator('Sova', '930526575319982133', true),
+    new Initiator('Breach', '930526575978500096'),
+    new Initiator('Skye', '930526575793930280'),
+    new Initiator('KAY/O', '930526575596810301'),
+    new Initiator('Fade', '1113104096593977446'),
+    new Initiator('Gecko', '1113104098770833520'),
     
-    new Controller('Brimstone', '', true),
-    new Controller('Omen', ''),
-    new Controller('Viper', ''),
-    new Controller('Harbor', ''),
-    new Controller('Astra', ''),
+    new Controller('Brimstone', '930526575299027034', true),
+    new Controller('Omen', '930526575311605781'),
+    new Controller('Viper', '930526575626178771'),
+    new Controller('Harbor', '1113104105804669021'),
+    new Controller('Astra', '930526576028827748'),
 
-    new Sentinel('Sage', '', true),
-    new Sentinel('Cypher', ''),
-    new Sentinel('Killjoy', ''),
-    new Sentinel('Deadlock', ''),
-    new Sentinel('Chamber', '')
+    new Sentinel('Sage', '930526575366140044', true),
+    new Sentinel('Cypher', '930526575248687165'),
+    new Sentinel('Killjoy', '930526575982706798'),
+    new Sentinel('Deadlock', '1204070658896822292'),
+    new Sentinel('Chamber', '930526575332573235')
 ]
